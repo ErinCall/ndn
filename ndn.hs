9c3e1980e92a6cfcbd91a1198a1b5fb5b7f683d8
Andrew Lorente <andrew.lorente@gmail.com>
2013-01-27 14:01:27 -0800

    Actually roll the dice in ndn

--- ndn.hs
+++ ndn.hs
@@ -1,15 +1,17 @@
 import System.Environment
+import System.Random
 
 main = do
     expression <- getArgs
-    putStrLn $ show $ ndn expression
+    gen <- getStdGen
+    putStrLn $ show $ ndn expression gen
 
-ndn :: [String] -> Int
+ndn :: (RandomGen g) => [String] -> g -> Int
 ndn (numDice : _ : dieSize : _) = rollDice (read numDice) (read dieSize)
 
-rollDice :: Int -> Int -> Int
-rollDice 1 dieSize = rollDie dieSize
-rollDice n dieSize = (rollDie dieSize) + (rollDice (n - 1) dieSize)
+rollDice :: (RandomGen g) => Int -> Int -> g -> Int
+rollDice 1 dieSize g = rollDie dieSize g
+rollDice n dieSize g = (rollDie dieSize g) + (rollDice (n - 1) dieSize g)
 
-rollDie :: Int -> Int
-rollDie dieSize = dieSize
+rollDie :: (RandomGen g) => Int -> g -> Int
+rollDie dieSize = fst . randomR (1, dieSize)
