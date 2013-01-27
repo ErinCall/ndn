58aa7181b49eb60c08dfd8a301576618a3ff8835
Andrew Lorente <andrew.lorente@gmail.com>
2013-01-27 14:10:07 -0800

    Don't recycle a used random-generator

--- ndn.hs
+++ ndn.hs
@@ -11,7 +11,11 @@ ndn (numDice : _ : dieSize : _) = rollDice (read numDice) (read dieSize)
 
 rollDice :: (RandomGen g) => Int -> Int -> g -> Int
 rollDice 1 dieSize g = rollDie dieSize g
-rollDice n dieSize g = (rollDie dieSize g) + (rollDice (n - 1) dieSize g)
+rollDice n dieSize g = let (roll, newGen) = rollDie' dieSize g
+                       in roll + (rollDice (n - 1) dieSize newGen)
 
 rollDie :: (RandomGen g) => Int -> g -> Int
-rollDie dieSize = fst . randomR (1, dieSize)
+rollDie size = fst . rollDie' size
+
+rollDie' :: (RandomGen g) => Int -> g -> (Int, g)
+rollDie' dieSize = randomR (1, dieSize)
